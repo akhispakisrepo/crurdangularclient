a10cb4b4f4ee5f8192e3695c6357bb3abdc21edd
Rajib Hossain <j2ee1172301@gmail.com>
2019-03-22 09:11:59 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerSearchComponent } from './customer-search/customer-search.component';
+import { SearchCustomerComponent } from './search-customer/search-customer.component';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCustomerComponent,
+    CustomerDetailsComponent,
+    CustomerListComponent,
+    CustomerSearchComponent,
+    SearchCustomerComponent,
+    SearchCustomersComponent,
+    CustomersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
